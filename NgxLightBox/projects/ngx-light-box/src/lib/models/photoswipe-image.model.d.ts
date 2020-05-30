13e1252ec220ccdb5893023b7dce0a8e3bd8d8bb
Akash Singh <akashs@datetheramp.com>
2020-05-30 11:33:47 +0530

    Rename The Files Due To NPM conflict

--- NgxLightBox/projects/ngx-light-box/src/lib/models/photoswipe-image.model.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class PhotoswipeImage {
-	src: string;
-	w: number;
-	h: number;
-	constructor(src: string, w: number, h: number);
-}
--- NgxLightBox/projects/ngx-light-box/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of ngx-light-box
- */
-
-export * from './lib/service/lightbox.service';
-export * from './lib/component/lightbox.component';
-export * from './lib/models/image.model';
-export * from './lib/ngxLightBoxModule';
-export * from './lib/models/photoswipe-interface';
